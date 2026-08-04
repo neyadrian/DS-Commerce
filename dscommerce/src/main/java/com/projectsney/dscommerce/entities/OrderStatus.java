205c5021394816ae71ea20de17c5b8e39c86668a
neyadrian <neyadrian2018@gmail.com>
2026-08-04 10:24:12 -0300

    Order, Enum, relacionamento muitos-para-um

---
+++ dscommerce/src/main/java/com/projectsney/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.projectsney.dscommerce.entities;
+
+public enum OrderStatus {
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+}
--- dscommerce/src/main/java/com/projectsney/dscommerce/entities/User.java
+++ dscommerce/src/main/java/com/projectsney/dscommerce/entities/User.java
@@ -1,8 +1,11 @@
 package com.projectsney.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.aspectj.weaver.ast.Or;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -16,6 +19,9 @@ public class User {
     private String phone;
     private LocalDate birthdate;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
 
@@ -78,4 +84,8 @@ public class User {
         this.password = password;
     }
 
+
+    public List<Order> getOrders() {
+        return orders;
+    }
 }
