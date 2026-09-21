f0f860053a6fdcef47b6de517796fb5064dfd9b2
neyadrian <neyadrian2018@gmail.com>
2026-09-21 15:50:12 -0300

    criando ControllerExceptionHandler

---
+++ dscommerce/src/main/java/com/projectsney/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.projectsney.dscommerce.controllers.handlers;
+
+import com.projectsney.dscommerce.dto.CustomError;
+import com.projectsney.dscommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> recurseNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
