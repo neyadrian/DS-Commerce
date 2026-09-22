7bf76cc689e77763ced0de7397c566c9f24ccc6a
neyadrian <neyadrian2018@gmail.com>
2026-09-22 19:10:23 -0300

    atualizando ControllerExceptionHandler

--- dscommerce/src/main/java/com/projectsney/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ dscommerce/src/main/java/com/projectsney/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.projectsney.dscommerce.controllers.handlers;
 
 import com.projectsney.dscommerce.dto.CustomError;
+import com.projectsney.dscommerce.services.exceptions.DatabaseException;
 import com.projectsney.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,13 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomError> recurseNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
