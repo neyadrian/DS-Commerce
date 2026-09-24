d5643099310a2b6a52233bf2c07af7c542880fb3
neyadrian <neyadrian2018@gmail.com>
2026-09-24 18:36:35 -0300

    atualizando product controller

--- dscommerce/src/main/java/com/projectsney/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/projectsney/dscommerce/controllers/ProductController.java
@@ -4,6 +4,7 @@ import com.projectsney.dscommerce.dto.CustomError;
 import com.projectsney.dscommerce.dto.ProductDTO;
 import com.projectsney.dscommerce.services.ProductService;
 import com.projectsney.dscommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -35,7 +36,7 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+    public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(dto.getId()).toUri();
@@ -43,7 +44,7 @@ public class ProductController {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+    public ResponseEntity<ProductDTO> update(@Valid @PathVariable Long id, @RequestBody ProductDTO dto) {
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
     }
